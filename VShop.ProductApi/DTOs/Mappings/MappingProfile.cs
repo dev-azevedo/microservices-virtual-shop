c6236428d01da2b84a4cc093c7ea851796a60bcf
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-09-11 12:34:24 -0300

    feat: implementing DTOs and Repositories

---
+++ VShop.ProductApi/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs.Mappings;
+
+public class MappingProfile :  Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+    
+}
