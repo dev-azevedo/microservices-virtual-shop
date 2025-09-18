93580730b79b272cbde25c408d448fcf926141f7
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-09-18 13:11:23 -0300

    fix: vshop.web files

--- VShop.Web/Services/ProductService.cs
+++ VShop.Web/Services/ProductService.cs
@@ -1,6 +1,46 @@
-﻿namespace VShop.Web.Services;
+﻿using System.Text.Json;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
 
-public class ProductService
+namespace VShop.Web.Services;
+
+public class ProductService : IProductService
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+    private const string apiEndpoint = "/api/products/";
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private ProductViewModel _productVM;
+    private IEnumerable<ProductViewModel> _productsVM;
+
+    public ProductService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        _jsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+    }
     
+
+    public Task<IEnumerable<ProductViewModel>> GetAllProducts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> FindProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> CreateProduct(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductViewModel> UpdateProduct(ProductViewModel productVM)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteProductById(int id)
+    {
+        throw new NotImplementedException();
+    }
 }
